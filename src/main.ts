81714f420852921bc9468f6f09891bee31887b31
Mick Jasker <mick.jasker@mediamonks.com>
2019-09-03 22:29:49 +0200

    Add PWA support

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import components from '@/components/global';
 import './firebase';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
