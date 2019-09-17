64db08550a96cdbdb7420ff07dd8ae281a1e0d75
Mick Jasker <mick.jasker@mediamonks.com>
2019-09-17 20:40:11 +0200

    Create api endpoint for sending messages to Dialogflow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as firebase from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
 import * as serviceAccount from './serviceAccount.json';
+import { SessionsClient } from 'dialogflow';
 
 // @ts-ignore
 const serviceKey = <AppOptions.credential>serviceAccount;
@@ -15,12 +16,26 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://localhost:8080'],
+    origin: ['https://localhost:8080', 'https://mickjasker.dev'],
   }),
 );
 
 app.get('/ping', (req, res) => {
   res.send('ping!');
+});
+
+app.post('/message', async (req, res) => {
+  const { queryInput, sessionId } = req.body;
+  const sessionClient = new SessionsClient({ credentials: serviceAccount });
+  const session = sessionClient.sessionPath('porfolio-bot-ipwpmm', sessionId);
+  sessionClient
+    .detectIntent({ session, queryInput })
+    .then(responses => {
+      res.send(responses[0].queryResult);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    });
 });
 
 exports.app = functions.https.onRequest(app);
